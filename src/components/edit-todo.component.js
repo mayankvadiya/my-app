48c4221a42eb0dfae215c7e5e5a3bb471f3b1b89
Mayank Vadiya <mayankvadiya14193@gmail.com>
2020-11-05 20:22:29 +0530

    Remove console message

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -64,7 +64,6 @@ export default class TodoEdit extends Component{
         }
         console.log("Object---->",obj);
         axios.post('http://localhost:5000/todos/update/'+this.props.match.params.id,obj).then(res=>{
-            console.log("result--->",res.data);
             NotificationManager.success('Todo updated successfully','success')
             this.props.history.push("/")
         }).catch(err=>{
